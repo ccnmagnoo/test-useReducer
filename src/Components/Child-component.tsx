e06b59ea98a7abcf48d7dae69cba2c088af09e74
Carlos Campos <41976110+ccnmagnoo@users.noreply.github.com>
2021-09-15 12:56:34 -0300

    context consumption

--- src/Components/Child-component.tsx
+++ src/Components/Child-component.tsx
@@ -1,14 +1,19 @@
 import React from 'react';
-import { fruits } from './data';
+import { MyContext } from './contextProvider';
+
 import useList from './useList';
 
 export const Child = () => {
-  const list = useList(fruits);
+  //consumind data from context
+  const data = React.useContext(MyContext);
+  //custom hook 👨‍🍳
+  const [list, size] = useList(data?.fruits);
   return (
     <>
       <div className='Child-section'>
         <h4>Child component</h4>
         <div className='Data-section'>{list}</div>
+        <div className='Data-section'>size: {size}</div>
       </div>
     </>
   );
--- src/Components/Parent-component.tsx
+++ src/Components/Parent-component.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
 import { Child } from './Child-component';
-import { vegetales } from './data';
+import { MyContext } from './contextProvider';
 import useList from './useList';
 
 export const Parent = () => {
-  //personliced hook returing  <ul> list
-  const list = useList(vegetales);
+  //data context consumption
+  const data = React.useContext(MyContext);
+  //custom hook 👨‍🍳
+  const [list, size] = useList(data?.vegetales);
 
   return (
     <section className='Parent-section'>
       <h3>Parent component</h3>
       <div className='Data-section'>{list}</div>
+      <div className='Data-section'>size: {size}</div>
       <Child />
     </section>
   );
